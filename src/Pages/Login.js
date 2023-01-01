b668b0321363db3d87ec186011e9519c429ce701
timothydn2003 <87452000+timothydn2003@users.noreply.github.com>
2022-12-31 17:02:53 -0800

    input

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -8,6 +8,13 @@ import AddCircleOutlinedIcon from '@mui/icons-material/AddCircleOutlined';
 import Button from '@mui/material/Button';
 
 const Login = () => {
+  const[name,setName] = useState('')
+  const[description,setDescription] = useState('')
+  const[languages,setLanguages] = useState('')
+  const[gitLink,setGitLink] = useState('')
+  const[link,setLink] = useState('')
+  const[image,setImage] = useState('')
+
   const stop = (event) => {
     event.preventDefault()
   }
@@ -20,28 +27,28 @@ const Login = () => {
         <div className='login-form-inputs'>
           <Row>
             <Col>
-              <TextField id="outlined-basic" label="Title" variant="outlined" required/>
+              <TextField id="outlined-basic" label="Title" variant="outlined" onChange={(e) => setName(e.target.value)} required/>
             </Col>
             <Col>
-              <TextField id="outlined-basic" label="Description" variant="outlined" required/>
-            </Col>
-          </Row>
-          <br></br>
-          <Row>
-            <Col>
-              <TextField id="outlined-basic" label="Languages" variant="outlined" required/>
-            </Col>
-            <Col>
-              <TextField id="outlined-basic" label="Website Link" variant="outlined"/>
+              <TextField id="outlined-basic" label="Description" variant="outlined" onChange={(e) => setDescription(e.target.value)} required/>
             </Col>
           </Row>
           <br></br>
           <Row>
             <Col>
-              <TextField id="outlined-basic" label="GitHub Link" variant="outlined" required/>
+              <TextField id="outlined-basic" label="Languages" variant="outlined" onChange={(e) => setLanguages(e.target.value)} required/>
             </Col>
             <Col>
-              <TextField id="outlined-basic" label="Image" variant="outlined" required/>
+              <TextField id="outlined-basic" label="Website Link" variant="outlined" onChange={(e) => setLink(e.target.value)}/>
+            </Col>
+          </Row>
+          <br></br>
+          <Row>
+            <Col>
+              <TextField id="outlined-basic" label="GitHub Link" variant="outlined" onChange={(e) => setGitLink(e.target.value)} required/>
+            </Col>
+            <Col>
+              <TextField id="outlined-basic" label="Image" variant="outlined" onChange={(e) => setImage(e.target.value)} required/>
             </Col>
           </Row>
           <br></br>
