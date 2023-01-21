21c416dab66e875728368539b1a427f2e8bf95c4
Timothy Doan <87452000+timothydn2003@users.noreply.github.com>
2023-01-21 01:30:13 -0800

    Update Projects.js

--- src/Pages/Sections/Projects.js
+++ src/Pages/Sections/Projects.js
@@ -42,8 +42,8 @@ function Projects() {
                   <div className='project-info'>
                     <Row>
                         <Col className='buttons'>
-                          {data.link!== "" || data.videoLink !== ""?<a href= {data.link != null? data.link: data.videLink} target = "_blank"><button className='button-icon' style={{ color: "black", fontSize: 15,  marginRight: '0px'}}><LanguageIcon/></button></a>:""}
-                          <a href = {data.gitLink} target = "_blank"><button className='button-icon'><GitHubIcon style={{ color: "black", fontSize: 35}}/></button></a>
+                          {data.link!== "" || data.videoLink !== ""?<a href= {data.link != null? data.link: data.videLink} target = "_blank"><button className='button-icon' style={{ color: "black", fontSize: 15}}><LanguageIcon/></button></a>:""}
+                          <a href = {data.gitLink} target = "_blank"><button className='button-icon'><GitHubIcon style={{ color: "black", fontSize: 15}}/></button></a>
                         </Col>
                     </Row>
                     <Row>
